03f4961da973b10749e28c7405c0bf5149b0285f
Manu <manuelguerrero@paucasesnovescifp.cat>
2024-12-03 15:43:05 +0100

    Terminada parte 2 R08

--- Piano/esquelet/assets/js/partitures.js
+++ Piano/esquelet/assets/js/partitures.js
@@ -48,12 +48,42 @@ for (let partitura of registros){
     td = document.createElement("td");
     tr.appendChild(td);
     for (let [key, valor] of Object.entries(acciones)){
-        icono = document.createElement("i");
-        td.appendChild(icono);
-        icono.classList = valor;
         boton = document.createElement("button");
         td.appendChild(boton);
-        boton.innerHTML = key;
+        icono = document.createElement("i");
+        boton.appendChild(icono);
+        icono.classList = valor;
+        boton.innerHTML += key;
+        if (key == "Borrar") {
+            boton.onclick = esborrarElement;
+        } else {
+            boton.onclick = modificarElement;
+        }
     }
 
 }
+
+function esborrarElement(event){
+    let tr = event.target.closest("tr");
+    let tbody = tr.closest("tbody");
+    let indice = Array.from(tbody.children).indexOf(tr) + 1;
+    if (window.confirm(`Està segur que vol esborrar l'element ${indice}?`)){
+        window.alert("Element esborrat!");
+    } else {
+        window.alert("Has cancel·lat l'acció");
+    }
+}
+
+function modificarElement(event){
+
+}
+
+function obrirLogin() {
+    let width = window.innerWidth / 2;  // Ancho de la ventana emergente
+    let height = window.innerHeight / 2; // Altura de la ventana emergente
+    let top = window.screenY + (window.innerHeight / 2 - height / 2); // Centro vertical relativo a la ventana actual
+    let left = window.screenX + (window.innerWidth / 2 - width / 2); // Centro horizontal relativo a la ventana actual
+
+    window.open("login.html", "Login", `width=${width},height=${height},top=${top},left=${left}`);
+}
+
